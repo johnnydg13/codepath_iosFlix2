63b065c1906a0fd29344468d89c03d75d88a5cb9
johnnydg13 <88180889+johnnydg13@users.noreply.github.com>
2022-03-13 23:21:38 -0700

    refactored

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by JDG on 3/11/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
